ec65982e4d40435b43376cc27605ab3d504400bd
Hannah Robinson <hannah.robinson.git@gmail.com>
2023-03-26 21:24:02 +1300

    Import 'Dogs' CSS Scroll Snap Carousel

---
+++ carousels-sliders/css-scroll-snap-carousel-shopify/main.js
@@ -0,0 +1,21 @@
+// Start replacement for Shopify data and liquid loop
+
+const dogs = [
+  'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  'https://images.unsplash.com/photo-1444212477490-ca407925329e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2128&q=80',
+  'https://images.unsplash.com/photo-1536809188428-e8ecf663d0be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  'https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  'https://images.unsplash.com/photo-1554456854-55a089fd4cb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  'https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+]
+
+const slider = document.querySelector('.slider__slides')
+
+dogs.forEach((dog) =>
+  slider.insertAdjacentHTML(
+    'beforeend',
+    `<figure class="slider__slide" data-slide><img src="${dog}" alt="great dog"></figure>`
+  )
+)
+
+// End replacement for Shopify data and liquid loop
